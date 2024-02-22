56e5f5f5d6485dce505350defcd4e05b269e4c93
noorksangha <113377440+noorksangha@users.noreply.github.com>
2024-02-21 19:35:33 -0800

    Update spd.py

--- EchoQuizzer/spd.py
+++ EchoQuizzer/spd.py
@@ -1,17 +1,26 @@
+import subprocess
 from pyannote.audio import Pipeline
 from pydub import AudioSegment
 import whisper
 import numpy as np
 import gc
 
-pipeline = Pipeline.from_pretrained(
-    "pyannote/speaker-diarization-3.1", use_auth_token='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')
+# Path to the original audio file
+# original_audio_path = "C:\\Users\\16043\\Desktop\\whisper\\recording.wav"
+original_audio_path = "C:\\Users\\16043\\Downloads\\recording.wav"
+# Path to the converted audio file
+converted_audio_path = "C:\\Users\\16043\\Desktop\\whisper\\output.wav"
+# Path to the transcription file
+transcription_file_path = "C:\\Users\\16043\\Desktop\\whisper\\tr_file.txt"
 
+# Convert the audio file to the desired format and sample rate using ffmpeg
+subprocess.run(["ffmpeg", "-i", original_audio_path, "-acodec", "pcm_s16le", "-ar", "44100", converted_audio_path], check=True)
+
+pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token='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')
 
 def read(k):
     y = np.array(k.get_array_of_samples())
     return np.float32(y) / 32768
-
 
 def millisec(timeStr):
     try:
@@ -19,36 +28,30 @@ def millisec(timeStr):
         return int(int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2])) * 1000
     except ValueError:
         print(f"Invalid time format: {timeStr}")
-        return 0  # or appropriate error handling
+        return 0
 
-
-k = str(pipeline(
-    "C:\\Users\\16043\\Desktop\\whisper\\test.wav")).split('\n')
+# Use the converted file for further processing
+k = str(pipeline(converted_audio_path)).split('\n')
 
 del pipeline
 gc.collect()
 
-audio = AudioSegment.from_wav(
-    "C:\\Users\\16043\\Desktop\\whisper\\test.wav")
+audio = AudioSegment.from_wav(converted_audio_path)
 audio = audio.set_frame_rate(16000)
 
 model = whisper.load_model("small.en")
 
 for l in range(len(k)):
     j = k[l].split(" ")
-    start = int(millisec(j[1].rstrip(']')))  # remove trailing ']'
+    start = int(millisec(j[1].rstrip(']')))
     end = int(millisec(j[4].rstrip(']')))
 
     tr = read(audio[start:end])
 
     result = model.transcribe(tr, fp16=False)
 
-    f = open("C:\\Users\\16043\\Desktop\\whisper\\tr_file.txt", "a")
-    f.write(f'\n[ {j[1]} -- {j[3]} ] {j[6]} : {result["text"]}')
-    f.close()
+    with open(transcription_file_path, "a") as f:
+        f.write(f'\n[ {j[1]} -- {j[3]} ] {j[6]} : {result["text"]}')
 
-    del f
-    del result
-    del tr
-    del j
+    del result, tr, j
     gc.collect()
